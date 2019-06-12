62a092f544790032118af834e2af4ac86bf3cf04
Quang Nguyen <quangn2595@gmail.com>
2019-06-12 09:01:25 -0700

    adds comments to the JS code and update the ReadMe

--- js/app.js
+++ js/app.js
@@ -3,6 +3,14 @@
 var time = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
 var customerControlCurve = [0.5, 0.75, 1.0, 0.6, 0.8, 1.0, 0.7, 0.4, 0.6, 0.9, 0.7, 0.5, 0.3, 0.4, 0.6];
 
+
+// This is constructor function that stores common properties among the store
+// minNumStaffPerHour = minimum number of staff working at every hour 
+// numCustomerPerStaff = number of customers that one staff member can handle
+// totalCookiesPerDay = the sum of all the cookies sold from 6am to 8pm for one store
+// cookieSaleHour = array that holds the number of cookies sold per hour for one store
+// customerHour = array that holds the number of customers per hour for one store
+// numStaffPerHour = array that holds the number of staff members at every hour for one store
 function Store(location, minCustomer, maxCustomer, avgCookiesPerHour) { 
   this.location = location;
   this.minCustomer = minCustomer;
@@ -17,7 +25,10 @@ function Store(location, minCustomer, maxCustomer, avgCookiesPerHour) {
   Store.list.push(this);
 }
 
+// This is the constructor's prototype that holds common functions that are accessible to all location of the Store constructor
 Store.prototype = { 
+
+  // Calculate cookies sold per hour and push it into the array cookieSaleHour. Calculation uses the control curve for the max customer at every hour and use that max to calculate the random number of customers which then use to calculate number of cookie sold per hour. 
   cookiesPerHour: function() { 
     var numCustomer;
     var numCookies;
@@ -33,6 +44,8 @@ Store.prototype = {
     }
     this.totalCookiesPerDay = this.cookieSaleHour.reduce(sum);
   },
+
+  // Calculate number of staff needed to work at every hour and store it in the array numStaffPerHour
   staffPerHour: function() { 
     var numStaff;
     for (var i = 0; i < time.length; i++) {
@@ -45,6 +58,8 @@ Store.prototype = {
       }
     }
   },
+
+  // Render the information for the cookies sold per hour for each store to the sales.html in a table format. 
   cookieRender: function() { 
     var tableBody = document.getElementById('cookie-table-body');
     var tableRow = document.createElement('tr');
@@ -65,6 +80,8 @@ Store.prototype = {
 
     tableBody.appendChild(tableRow);
   },
+
+  // Render the information for the amount of staff per hour for each store to the sales.html in a table format. 
   staffRender: function() { 
     var tableBody = document.getElementById('staff-table-body');
     var tableRow = document.createElement('tr');
@@ -87,6 +104,7 @@ Store.prototype = {
   }
 }
 
+// Render the header row that contains the times of each hour of the table for both cookies sold per hour and the amount of staff per hour. Takes in an id which indicates which table to put the header in.
 function headerRow(id) { 
   var tableHeader = document.getElementById(id);
   var tableRow = document.createElement('tr');
@@ -108,6 +126,7 @@ function headerRow(id) {
   tableHeader.appendChild(tableRow);
 }
 
+// Render the footer row to show the sum of the cookies sold per hour for every store. Also calculate the sum of cookies sold per hour of every store.   
 function cookieFooterRow() { 
   var tableFooter = document.getElementById("cookie-table-foot");
   var tableRow = document.createElement('tr');
@@ -138,6 +157,7 @@ function cookieFooterRow() {
   tableFooter.appendChild(tableRow);
 }
 
+// Calculate and render the sum of the hours worked by staff at every hour for every store. 
 function staffFooterRow() { 
   var tableFooter = document.getElementById("staff-table-foot");
   var tableRow = document.createElement('tr');
@@ -172,6 +192,8 @@ function sum(total, num) {
   return total + num;
 }
 
+
+// Initializing the store list to have every new instance of Store be put inside the list
 Store.list = [];
 
 new Store('1st and Pike', 23, 65, 6.3);
@@ -180,18 +202,23 @@ new Store('Seattle Center', 11, 38, 3.7);
 new Store('Capitol Hill', 20, 38, 2.3);
 new Store('Alki', 2, 16, 4.6);
 
+// Call the functions to calculate the cookies per hour and staff per hour for every store.
 for (var i = 0; i < Store.list.length; i++) { 
   Store.list[i].cookiesPerHour();
   Store.list[i].staffPerHour();
 }
 
 headerRow("cookie-table-head");
+
+// Use the cookie render function for every store
 for (var i = 0; i < Store.list.length; i++) { 
   Store.list[i].cookieRender();
 }
 cookieFooterRow();
 
 headerRow("staff-table-head");
+
+// Use the staff render function for every store.
 for (var i = 0; i < Store.list.length; i++) { 
   Store.list[i].staffRender();
 }
